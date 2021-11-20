541d9fcea8ad34fdc99348010d4f46404b7213ef
govind6498 <govindsingh6498@gmail.com>
2021-11-20 08:10:38 +0530

    Added:To initialize project structure

---
+++ src/main/java/com/bridgelabz/messesingapp/MessesingAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.messesingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessesingAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Govind Kumar");
+		SpringApplication.run(MessesingAppApplication.class, args);
+		
+	}
+}
